03a8c496e19dd46561e5253dca37c8448a7e4100
Ryan Courtney <ryan.courtney1@gmail.com>
2017-06-05 13:09:53 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20170208160602) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "event_created"
     t.integer  "comment_created"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "content"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -45,8 +47,10 @@ ActiveRecord::Schema.define(version: 20170208160602) do
   end
 
   create_table "followers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "follower_id"
+    t.integer  "following_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "following"
     t.integer  "follower"
   end
@@ -70,8 +74,8 @@ ActiveRecord::Schema.define(version: 20170208160602) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
